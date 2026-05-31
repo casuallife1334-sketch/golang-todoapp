e6e6e7b1f5f7a5fa6bbb8bbbdf5689c5a88d6770
noperks <casuallife1334@gmail.com>
2026-05-31 20:52:06 +0300

    fixes

--- cmd/todoapp/main.go
+++ cmd/todoapp/main.go
@@ -7,7 +7,7 @@ import (
 	"syscall"
 
 	core_logger "github.com/casuallife1334-sketch/golang-todoapp/internal/core/logger"
-	core_postgres_pool "github.com/casuallife1334-sketch/golang-todoapp/internal/core/repository/postgres/pool"
+	core_pgx_pool "github.com/casuallife1334-sketch/golang-todoapp/internal/core/repository/postgres/pool/pgx"
 	core_http_middleware "github.com/casuallife1334-sketch/golang-todoapp/internal/core/transport/http/middleware"
 	core_http_server "github.com/casuallife1334-sketch/golang-todoapp/internal/core/transport/http/server"
 	users_postgres_repository "github.com/casuallife1334-sketch/golang-todoapp/internal/features/users/repository/postgres"
@@ -30,9 +30,10 @@ func main() {
 	defer logger.Close()
 
 	logger.Debug("initializing postgres connection pool")
-	pool, err := core_postgres_pool.NewConnectionPool(
+
+	pool, err := core_pgx_pool.NewPool(
 		ctx,
-		core_postgres_pool.NewConfigMust(),
+		core_pgx_pool.NewConfigMust(),
 	)
 	if err != nil {
 		logger.Fatal("failed to init postgres connection pool", zap.Error(err))
@@ -51,13 +52,23 @@ func main() {
 		logger,
 		core_http_middleware.RequestID(),
 		core_http_middleware.Logger(logger),
-		core_http_middleware.Panic(),
 		core_http_middleware.Trace(),
+		core_http_middleware.Panic(),
 	)
 
-	apiVersionRouter := core_http_server.NewApiVersionRouter(core_http_server.ApiVersion1)
-	apiVersionRouter.RegisterRoutes(usersTransportHTTP.Routes()...)
-	httpServer.RegisterAPIRouters(apiVersionRouter)
+	apiVersionRouterV1 := core_http_server.NewApiVersionRouter(core_http_server.ApiVersion1)
+	apiVersionRouterV1.RegisterRoutes(usersTransportHTTP.Routes()...)
+
+	// apiVersionRouterV2 := core_http_server.NewApiVersionRouter(
+	// 	core_http_server.ApiVersion2,
+	// 	core_http_middleware.Dummy("api v2 middleware"),
+	// )
+	// apiVersionRouterV2.RegisterRoutes(usersTransportHTTP.Routes()...)
+
+	httpServer.RegisterAPIRouters(
+		apiVersionRouterV1,
+		// apiVersionRouterV2,
+	)
 
 	if err := httpServer.Run(ctx); err != nil {
 		logger.Error("HTTP server run error:", zap.Error(err))
